abdc8b3221c6f298fcaf923f07ccf47fe003099f
Paul Sitoh <paulwizviz@gmail.com>
2022-03-28 20:49:52 +0100

    Refactor trie package
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- pkg/trie/trie.go
+++ pkg/trie/trie.go
@@ -6,7 +6,6 @@ package trie
 
 import (
 	"fmt"
-	"unicode"
 )
 
 const (
@@ -44,60 +43,6 @@ type Node interface {
 
 	// Runes returns the edges connected to this node
 	Runes() []rune
-}
-
-// This is not serializable
-type defaultNode struct {
-	edges map[rune]Node
-}
-
-func (d *defaultNode) InsertRune(data rune) Node {
-	lrune := unicode.ToLower(data)
-	node, ok := d.edges[lrune]
-	if !ok {
-		d.edges[lrune] = NewDefaultNode()
-		return d.edges[lrune]
-	}
-	return node
-}
-
-func (d *defaultNode) IsLeaf() bool {
-	return len(d.edges) == 0
-}
-
-func (d *defaultNode) MatchedRune(data rune) (isEnd bool, isMatched bool) {
-	isMatched, isEnd = false, false
-	nextNode, ok := d.edges[data]
-	if !ok {
-		return isEnd, isMatched
-	}
-	isEnd, isMatched = nextNode.IsLeaf(), true
-	return isEnd, isMatched
-}
-
-func (d *defaultNode) NextNode(ch rune) (node Node) {
-	node, ok := d.edges[ch]
-	if !ok {
-		return nil
-	}
-	return node
-}
-
-func (d *defaultNode) Runes() []rune {
-	var runes []rune
-
-	for r, _ := range d.edges {
-		runes = append(runes, r)
-	}
-
-	return runes
-}
-
-// NewDefaultNode instantiate a default Trie node.
-func NewDefaultNode() Node {
-	return &defaultNode{
-		edges: make(map[rune]Node),
-	}
 }
 
 // Populate operation to populate a Trie
